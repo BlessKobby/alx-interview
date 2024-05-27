47e2971f5ee4e24687d903c3dd8d12b2f3865c8b
BlessKobby <amablebless@gmail.com>
2024-05-27 16:13:50 +0000

    Pascal's Triangle.

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" A script to determine Pascal's triangle for any number """
+
+
+def pascal_triangle(n: int ):
+    """
+    returns a list of lists of integers representing the Pascal's triangle of n
+    """
+    triangle = []
+
+    # return (trianlgle if n <= 0)
+    if n <= 0:
+        return triangle
+    for u in range(n):
+        temp_list = []
+
+        for v in range(u+1):
+            if v == 0 or v == u:
+                temp_list.append(1)
+            else:
+                temp_list.append(triangle[u-1][v-1] + triangle[u-1][v])
+        triangle.append(temp_list)
+    # print(triangle)
+    return triangle
